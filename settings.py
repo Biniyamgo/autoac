0759c9c45c85768ea2f22a2f75253471d67d0c0d
bini <marta@Mac-mini.local>
2025-05-24 22:57:18 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,89 @@
+from configparser import ConfigParser
+import json
+from typing import List
+from dotenv import load_dotenv
+import os
+import logging
+# from keep_alive import keep_alive
+
+load_dotenv()
+#21129632
+API_ID = 10897219
+#tse f life 18488809
+#tse g life 03deff21f87a1a3d345f57f739b60465
+#yoni herbale api the same
+#21251941
+# moos 12069804
+#10897219+
+#17421852
+#
+#os.getenv('api_id')
+API_HASH = 'fb627bc739f63f5f61eb2f5667a07445'
+#os.getenv('api_hash')
+STRING_SESSION = os.getenv('STRING_SESSION')
+
+assert API_ID and API_HASH
+
+configur = ConfigParser()
+
+# groups = [
+#   '@sheger1_gebeya', '@Home_Marketo', '@Goldn_mareket', '@bnshopping',
+#   '@golden_mareket1', '@marketineg', '@BHP_MARKET_CENTER', '@ethiomarts12',
+#   '@Moosmarket10', '@golden_mareket', '@Ethio_fast_market ', '@Ethi0_market',
+#   '@ahadumarket12', '@market_0nline', '@sellsgroup', '@market_center21',
+#   '@marketcenter11', '@ethioo_markett', '@sinamarket0', '@ethio_Gebeeya',
+#   '@norgebeya', '@Maahllet', '@AHADUMARKET122', '@Etmart1', '@samri077',
+#   '@zembilshoppingcenter', '@one1_Market', '@habeshamart_et',
+#   '@ethio_smart_Market', '@sina_gebeya1'
+# ]
+groups = [
+  '@Expresshop1', '@New_marketdv', '@Esssentstore', '@Express_shooping',
+  '@EssentialMarket', '@Merkatoahoppp', '@EliteBallers1', '@kingpromotion1',
+  '@kingpromotion2', '@kingpromotion3', '@kingpromotion4',
+  '@qualitymarketplaces', '@ethio_smart_Market', '@zembilshoppingcenter',
+  '@Etmart1', '@ethioexpres12', '@bnshopping', '@addis_shopp',
+  '@AHADUMARKET122', '@EthioEbay12', '@lehulummarket', '@Digital_Market_Team',
+  '@ethio_amazon_group2', '@SHEMETAZONE', '@NIGUSMARKET', '@market_0nline',
+  '@ahadumarket12', '@shegermarket21', '@norgebeya', '@SHEMETAZONE1',
+  '@yetm_yetm12', '@Elkexpress', '@ritimarketing', '@ADVERTISERS_GROUP',
+  '@abuuchuu', '@CTech_Promotion_Agency', '@marketineg', '@Gojo_Fashion',
+  '@adisssmarket', '@addisMarketBuyAnything2', '@addisgebeyaonline',
+  '@Gomarket12', '@abonlinemarket22', '@gulitonlinemarket2', '@enzosStore',
+  '@ethio_Gebeeya', '@ethioo_markett', '@golden_mareket1', '@Goldn_mareket',
+  '@sina_gebeya1', '@sinamarket0', '@onlineshop_45', '@addisMarketBuyAnything',
+  '@etmarketon', '@sartmart'
+]
+
+for d in groups:
+  title = str(d)
+  buna = {
+    title: {
+      'from': 'https://t.me/arada08',
+      'to': d,
+      'offset': 1,
+    }
+  }
+
+  configur.read_dict(buna)
+
+#ConfigParser()
+#configur.read()
+
+forwards = configur.sections()
+
+
+def get_forward(forward: str) -> tuple:
+  try:
+    from_chat = configur.get(forward, 'from')
+    to_chat = configur.get(forward, 'to')
+    offset = configur.getint(forward, 'offset')
+    return from_chat, to_chat, offset
+  except Exception as err:
+    print(err)
+
+
+if __name__ == "__main__":
+
+  #testing
+  for forward in forwards:
+    print(forward, get_forward(forward))
---
+++ telegram_account_bot.py
@@ -0,0 +1,124 @@
+''' A script to send all messages from one chat to another. '''
+
+import asyncio
+from audioop import reverse
+from configparser import ConfigParser
+from socks import SOCKS5, SOCKS4, HTTP
+import logging
+import time
+from telethon.tl.patched import MessageService
+from telethon.errors.rpcerrorlist import FloodWaitError
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from settings import API_ID, API_HASH, forwards, get_forward, STRING_SESSION
+from keep_alive import keep_alive
+
+#logging.basicConfig(
+#format='%(asctime)s - %(name)s - %(levelname)s - %##(message)s', level=logging.INFO)
+
+SENT_VIA = f'\n__Sent via__ `{str(__file__)}`'
+
+
+def intify(string):
+  try:
+    return int(string)
+  except:
+    return string
+
+
+async def forward_job():
+  ''' the function that does the job 😂 '''
+  if STRING_SESSION:
+    session = StringSession(STRING_SESSION)
+  else:
+    session = 'forwarder'
+
+  async with TelegramClient(
+      'user',
+      API_ID,
+      API_HASH,
+      # proxy={
+      #   'proxy_type': 'socks5',
+      #   'addr': '156.251.248.93',
+      #   'port': 33446,
+      # },
+  ) as client:
+
+    confirm = ''' IMPORTANT 🛑
+            Are you sure that your `config.ini` is correct ?
+
+            Take help of @userinfobot for correct chat ids.
+            
+            Press [ENTER] to continue:
+            '''
+
+    #input(confirm)
+    count = 0
+    value = True
+
+    # timeout variable can be omitted, if you use specific value in the while condition
+    #timeout = 519000 # [seconds]
+
+    #timeout_start = time.time()
+
+    while (value):
+      error_occured = False
+      number = [3]
+      for num in number:
+        for forward in forwards:
+          from_chat, to_chat, offset = get_forward(forward)
+
+          if not offset:
+            offset = 0
+
+          last_id = 0
+
+          async for message in client.iter_messages(intify(from_chat)):
+            if isinstance(message, MessageService):
+              continue
+
+            try:
+
+              #print(message.id, 'binii')
+              if message.id == num:
+
+                await client.forward_messages(intify(to_chat), message)
+                last_id = str(message.id)
+
+                #logging.info('forwarding message with id = %s', last_id)
+
+              # update_offset(forward, last_id)
+
+            except FloodWaitError as fwe:
+              #print(f'{fwe}')
+              asyncio.sleep(delay=fwe.seconds)
+            except Exception as err:
+              #logging.exception(err)
+              error_occured = True
+              break
+
+        count += 1
+        #print('post for', count, 'time')
+        delaytime = 1
+        for i in range(1, delaytime):
+          time.sleep(1)
+    await client.send_file(
+      'me',
+      'config.ini',
+      caption='This is your config file for telegram-chat-forward.')
+
+    message = 'Your forward job has completed.' if not error_occured else 'Some errors occured. Please see the output on terminal. Contact Developer.'
+    await client.send_message('me',
+                              f'''Hi !
+        \n**{message}**
+        \n**Telegram Chat Forward** is developed by @AahnikDaw.
+        \nPlease star 🌟 on [GitHub](https://github.com/aahnik/telegram-chat-forward).
+        {SENT_VIA}''',
+                              link_preview=False)
+
+
+keep_alive()
+if __name__ == "__main__":
+
+  assert forwards
+  asyncio.get_event_loop().run_until_complete(forward_job())
